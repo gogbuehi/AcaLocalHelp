fe4d9e375fdddfb63c842a242dbc799b43c9a3c7
Souleymane Coulibaly <bina.coulibaly@gmail.com>
2016-01-26 23:09:02 -0500

    Added layout for display location activity, and skeleton code for the activity.

--- app/src/main/java/com/justgoodwin/android/acalocalhelp/activities/DisplayLocationActivity.java
+++ app/src/main/java/com/justgoodwin/android/acalocalhelp/activities/DisplayLocationActivity.java
@@ -3,6 +3,11 @@ package com.justgoodwin.android.acalocalhelp.activities;
 import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.widget.SimpleCursorAdapter;
+import android.view.Gravity;
+import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.ProgressBar;
 
 /**
  * Created by Goodwin on 1/24/16.
@@ -10,17 +15,31 @@ import android.os.Bundle;
 public class DisplayLocationActivity extends ListActivity {
     public final static String INTENT_RESULTS_RESPONSE="RESULTS_RESPONSE";
     private String url = "https://localhelp.healthcare.gov/api/v1/resources/agents/all/38.8845126_-77.0938583.json?lat=38.8845126&lng=-77.0938583&medicaidState=VA&radius=50&order=proximity&limit=none&offset=0";
-
+    SimpleCursorAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        //Show progress bar
+        ProgressBar progressBar = new ProgressBar(this);
+        progressBar.setLayoutParams(new AbsListView.LayoutParams(AbsListView.LayoutParams.WRAP_CONTENT,
+                AbsListView.LayoutParams.WRAP_CONTENT, Gravity.CENTER));
+        progressBar.setIndeterminate(true);
+        getListView().setEmptyView(progressBar);
+
+        ViewGroup root = (ViewGroup) findViewById(android.R.id.content);
+        root.addView(progressBar);
+
+        String[] fromColumns = {};
+        int[] toViews = {android.R.id.text1};
+        SimpleCursorAdapter adapter = new SimpleCursorAdapter(this, android.R.id.text1, null, fromColumns, toViews);
+        setListAdapter(adapter);
+
         Intent intent = getIntent();
         if(intent.getAction().equals(INTENT_RESULTS_RESPONSE)) {
             //Deserialize data
         }
-
 
     }
 }
