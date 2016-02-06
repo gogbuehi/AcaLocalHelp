fb3396caabc961d7484a448e3748695e4b6a884a
Souleymane Coulibaly <bina.coulibaly@gmail.com>
2016-02-05 20:32:18 -0500

    Added HGov style guide fonts.

--- app/src/main/java/com/justgoodwin/android/acalocalhelp/MainActivity.java
+++ app/src/main/java/com/justgoodwin/android/acalocalhelp/MainActivity.java
@@ -3,14 +3,15 @@ package com.justgoodwin.android.acalocalhelp;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.location.Address;
 import android.location.Geocoder;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.justgoodwin.android.acalocalhelp.activities.DisplayLocationActivity;
@@ -20,13 +21,16 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Locale;
 
-/**
- * Created by Goodwin on 1/24/16.
- */
 public class MainActivity extends Activity {
     private EditText addressSearch;
     private Button searchButton;
+    private TextView introTitle;
+    private TextView introDescription;
+    private TextView introSearchLabel;
     public Geocoder geocoder;
+    public Typeface BITTER_REGULAR;
+    public Typeface OPEN_SANS_REGULAR;
+    public Typeface OPEN_SANS_BOLD;
     private Toast invalidSearchToast;
 
     @Override
@@ -34,11 +38,28 @@ public class MainActivity extends Activity {
         final Activity self = this;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        // Fonts
+        BITTER_REGULAR = Typeface.createFromAsset(getAssets(), getString(R.string.bitter_regular));
+        OPEN_SANS_REGULAR = Typeface.createFromAsset(getAssets(), getString(R.string.open_sans_regular));
+        OPEN_SANS_BOLD = Typeface.createFromAsset(getAssets(), getString(R.string.open_sans_bold));
+
+        // UI Elements
         addressSearch = (EditText) this.findViewById(R.id.addressSearch);
         searchButton = (Button) this.findViewById(R.id.searchButton);
-        Log.d("ACA","Button is: " + (searchButton == null ? "NULL": "NOT NULL"));
+        introTitle = (TextView) this.findViewById(R.id.introTitleTextView);
+        introDescription = (TextView) this.findViewById(R.id.introDiscriptionTextView);
+        introSearchLabel = (TextView) this.findViewById(R.id.introSearchLabelTextView);
+
+        introTitle.setTypeface(BITTER_REGULAR);
+        introDescription.setTypeface(OPEN_SANS_REGULAR);
+        introSearchLabel.setTypeface(OPEN_SANS_BOLD);
+        addressSearch.setTypeface(OPEN_SANS_REGULAR);
+        searchButton.setTypeface(OPEN_SANS_BOLD);
+
         geocoder = new Geocoder(this, Locale.getDefault());
         Context context = getApplicationContext();
+
         CharSequence text = "You must enter a valid City and State or Zip code.";
         int duration = Toast.LENGTH_SHORT;
         invalidSearchToast = Toast.makeText(context, text, duration);
